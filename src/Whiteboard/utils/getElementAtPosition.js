79ec0e79bb67ea3eb844357d5246be1f136a90a5
Bảo Bảo <baobao@Baos-MacBook-Pro.local>
2024-11-25 03:42:14 +0700

    Resizing And Moving Line

--- src/Whiteboard/utils/getElementAtPosition.js
+++ src/Whiteboard/utils/getElementAtPosition.js
@@ -1,5 +1,17 @@
 import { toolTypes, cursorPositions } from "../../constants";
 
+const distance = (a, b) =>
+  Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+
+const onLine = ({ x1, y1, x2, y2, x, y, maxDistance = 1 }) => {
+  const a = { x: x1, y: y1 };
+  const b = { x: x2, y: y2 };
+  const c = { x, y };
+
+  const offset = distance(a, b) - (distance(a, c) + distance(b, c));
+  return Math.abs(offset) < maxDistance ? cursorPositions.INSIDE : null;
+};
+
 const nearPoint = (x, y, x1, y1, cursorPosition) => {
   return Math.abs(x - x1) < 5 && Math.abs(y - y1) < 5 ? cursorPosition : null;
 };
@@ -22,6 +34,12 @@ const positionWithinElement = (x, y, element) => {
       return x >= x1 && x <= x2 && y >= y1 && y <= y2
         ? cursorPositions.INSIDE
         : null;
+    case toolTypes.LINE:
+      const on = onLine({ x1, y1, x2, y2, x, y });
+      const start = nearPoint(x, y, x1, y1, cursorPositions.START);
+      const end = nearPoint(x, y, x2, y2, cursorPositions.END);
+
+      return start || end || on;
   }
 };
 
